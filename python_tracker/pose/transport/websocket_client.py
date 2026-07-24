dcfd12d6145c317833107e4664a6569291e37aa4
srbislavpetrovic59-lang <srbislav.petrovic.59@gmail.com>
2026-07-24 16:31:20 +0200

    Add websockets dependency

--- python_tracker/pose/transport/websocket_client.py
+++ python_tracker/pose/transport/websocket_client.py
@@ -1,14 +1,26 @@
 class WebSocketClient:
     """Handles WebSocket transport."""
 
-    def __init__(self) -> None:
-        """Initializes the WebSocket client."""
+    def __init__(
+        self,
+        host: str,
+        port: int,
+    ) -> None:
+        """Initializes the client."""
+        self._host = host
+        self._port = port
 
     def connect(self) -> None:
+        """Opens the connection."""
         raise NotImplementedError
 
     def disconnect(self) -> None:
+        """Closes the connection."""
         raise NotImplementedError
 
-    def send(self, message: str) -> None:
+    def send(
+        self,
+        message: str,
+    ) -> None:
+        """Sends a serialized message."""
         raise NotImplementedError
