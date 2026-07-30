84d815ebf7b066df8001cc3c5317dbab78889ba9
srbislavpetrovic59-lang <srbislav.petrovic.59@gmail.com>
2026-07-30 16:07:43 +0200

    Initialize MediaPipe Hands detector

--- python_tracker/pose/hand_detector.py
+++ python_tracker/pose/hand_detector.py
@@ -1,6 +1,18 @@
+import cv2
+import mediapipe as mp
+
+
 class HandDetector:
 
     def __init__(self):
+        self._mp_hands = mp.solutions.hands
+        self._hands = self._mp_hands.Hands( 
+        static_image_mode=False,
+        max_num_hands=2,
+        model_complexity=1,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5,
+    )
         pass
 
     def process(self, frame):
