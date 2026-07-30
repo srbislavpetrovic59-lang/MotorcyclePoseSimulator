01846d701c2c28b4a8c01e044585aa7c0623358d
srbislavpetrovic59-lang <srbislav.petrovic.59@gmail.com>
2026-07-30 15:37:47 +0200

    Introduce HandDetector skeleton

--- python_tracker/pose/hand_detector.py
+++ python_tracker/pose/hand_detector.py
@@ -1,33 +1,10 @@
-# pose_detector.py
-
-import cv2
-import mediapipe as mp
-
-
-class PoseDetector:
+class HandDetector:
 
     def __init__(self):
+        pass
 
-        self._mp_pose = mp.solutions.pose
-        self._pose = self._mp_pose.Pose(
-            static_image_mode=False,
-            model_complexity=1,
-            smooth_landmarks=True,
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5
-        )
+    def process(self, frame):
+        return None
 
-    def detect(self, frame):
-
-        rgb_frame = cv2.cvtColor(
-            frame,
-            cv2.COLOR_BGR2RGB
-        )
-
-        results = self._pose.process(rgb_frame)
-
-        return results.pose_landmarks
-
-    def release(self):
-        self._pose.close()
-
+    def close(self):
+        pass
